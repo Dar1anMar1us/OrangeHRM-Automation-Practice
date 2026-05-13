0e9a35fac62180aba264b082296c314e8fd312f8
Darian Chirca <dmc.41155@gmail.com>
2026-05-13 19:35:13 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  viewportHeight: 860,
+  screenshotOnRunFailure: true,
+  screenshotQuality: 20, // lower quality, smaller files
+  video: false,
+  chromeWebSecurity: true,
+  includeShadowDom: true,
+  experimentalMemoryManagement: true,
+  numTestsKeptInMemory: 0,
+  experimentalWebKitSupport: false,
+  defaultCommandTimeout: 10000,
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
